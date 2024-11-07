082ac2d287d515cf995e3770db31dff45ebdb109
greenlemonT <ms950219@naver.com>
2024-11-07 22:53:41 +0900

    Fix: enum -> string

---
+++ src/main/java/myPharm/myPharm/domain/dto/medbox/MedboxResDto.java
@@ -0,0 +1,11 @@
+package myPharm.myPharm.domain.dto.medbox;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MedboxResDto {
+
+    private String medicineName;
+}
--- src/main/java/myPharm/myPharm/global/AlertEnum.java
+++
@@ -1,5 +0,0 @@
-package myPharm.myPharm.global;
-
-public enum AlertEnum {
-    병용금기, 노인주의, 특정연령대금기, 임부금기
-}
