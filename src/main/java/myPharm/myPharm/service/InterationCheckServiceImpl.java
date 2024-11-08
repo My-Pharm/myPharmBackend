7ab4bc5fe8ebe839e6ebeb2f882ff6764fbe1d59
kwonjieon <holemart@naver.com>
2024-11-08 15:38:49 +0900

    feat: interactioncheck기능

--- src/main/java/myPharm/myPharm/service/InterationCheckServiceImpl.java
+++ src/main/java/myPharm/myPharm/service/InterationCheckServiceImpl.java
@@ -1,53 +1,50 @@
 package myPharm.myPharm.service;
 
+import lombok.RequiredArgsConstructor;
 import myPharm.myPharm.domain.entity.IngredientEntity;
 import myPharm.myPharm.domain.entity.MedboxEntity;
+import myPharm.myPharm.repository.ProhibitRepository;
+import myPharm.myPharm.repository.RelationRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
 @Service
+@RequiredArgsConstructor
 public class InterationCheckServiceImpl implements InteractionCheckSercive{
-    public List<String[]> checkInteraction(List<MedboxEntity> medboxEntities){
-//        Set<String> medIngredient = new HashSet<>();
-//        List<String[]> alertList = new ArrayList<>();
-//        //medbox약이름 성분으로 변경
-//        List<IngredientEntity> list = new ArrayList<>();
-//
-//        for(IngredientEntity ingredient: list){
-//
-//            medIngredient.add(ingredient.getIngredientName());
-//        }
-//        //med의 금지 받아옴
-//        //List<IngredientEntity> against = new ArrayList<>();
+    private final RelationRepository relationRepository;
+    private final ProhibitRepository prohibitRepository;
+    private final DataService dataService;
 
+    public List<String[]> checkInteraction(List<MedboxEntity> medboxEntities){
 
         List<String[]> alertList = new ArrayList<>();//리턴값
         Map<String,String> medIngre = new HashMap();//성분 /약이름// ingredient테이블 medicine테이블 조인해서 가져옴 약기준으로 성분가져와서 반대로넣기
+        Map<String,String> prohibitRelation = new HashMap();//안되는성분 /약성분//릴레이션테이블에서 가져옴
 
-        Map<String,String> againstIngre = new HashMap();//안되는성분 /약성분//릴레이션테이블에서 가져옴
-        for(MedboxEntity med:medboxEntities){
-            //med의 금지 받아옴
-            List<IngredientEntity> ingres = new ArrayList<>();
-            for(IngredientEntity in:ingres) {
-                medIngre.put(in.getIngredientName(), med.getMedicine().getMedicineName());
+
+
+        for(MedboxEntity medbox:medboxEntities){
+
+            List<IngredientEntity> IngreList = relationRepository.findIngredientsByMedicineName(medbox.getMedicine().getMedicineName());
+            for(IngredientEntity ingredient:IngreList) {
+                medIngre.put(ingredient.getIngredientName(), medbox.getMedicine().getMedicineName());
                 //안되는거 뽑아옴
-                List<IngredientEntity> against = new ArrayList<>();//in기준
-                for(IngredientEntity aga :against){
-                    againstIngre.put(aga.getIngredientName(),in.getIngredientName());
+                List<String> prohibitIngredient = dataService.searchprohibit(ingredient.getIngredientName()).getProhibitedIngredients();
+                //List<IngredientEntity> prohibitIngredient = prohibitRepository.findByIngredient1_IngredientNameOrIngredient2_IngredientName();//변경예정    //in기준
+                for(String ingre :prohibitIngredient){
+                    prohibitRelation.put(ingre,ingredient.getIngredientName());//안되는성분 //내약성분
                 }
             }
-            for(String key:medIngre.keySet()){
-                if(againstIngre.containsKey(key)){
 
-                    alertList.add(new String[]{medIngre.get(againstIngre.get(key)), medIngre.get(key)});
-                }
-            }
 
         }
+        for(String key:medIngre.keySet()){
+            if(prohibitRelation.containsKey(key)){
 
-
-
+                alertList.add(new String[]{medIngre.get(prohibitRelation.get(key)), medIngre.get(key)});
+            }
+        }
 
 
         return alertList;
