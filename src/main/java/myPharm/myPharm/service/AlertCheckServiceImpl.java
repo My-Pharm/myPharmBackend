d8ca80853c397cf58e4c2d53351e9e3da4f3654d
kwonjieon <holemart@naver.com>
2024-11-10 00:15:00 +0900

    feat: alertcheck 현재 복용약만 검사

--- src/main/java/myPharm/myPharm/service/AlertCheckServiceImpl.java
+++ src/main/java/myPharm/myPharm/service/AlertCheckServiceImpl.java
@@ -1,6 +1,8 @@
 package myPharm.myPharm.service;
 
 import lombok.RequiredArgsConstructor;
+import myPharm.myPharm.domain.dto.alert.AlertResDto;
+import myPharm.myPharm.domain.dto.medbox.MedboxResDto;
 import myPharm.myPharm.domain.entity.AlertEntity;
 import myPharm.myPharm.domain.entity.IngredientEntity;
 import myPharm.myPharm.domain.entity.MedboxEntity;
@@ -8,29 +10,38 @@ import myPharm.myPharm.repository.AlertRepository;
 import myPharm.myPharm.repository.RelationRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class AlertCheckServiceImpl {
     private final AlertRepository alertRepository;
     private final RelationRepository relationRepository;
-    public List<String[]> alertCheck(List<MedboxEntity> medboxEntities){
+    public List<AlertResDto> alertCheck(List<MedboxEntity> medboxEntities) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        sdf.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+        String formattedDate = sdf.format(new Date());
+
+        // 한국 시간으로 변환된 Date 객체
+        Date curDate = sdf.parse(formattedDate);
+        medboxEntities = medboxEntities.stream()
+                .filter(medbox -> medbox.getEndDate().after(curDate) || medbox.getEndDate().equals(curDate))
+                .collect(Collectors.toList());
 
         //List<String[]> alertList = new ArrayList<>();//리턴값
-        List<String[]> alertList = new ArrayList<>();
+        List<AlertResDto> alertList = new ArrayList<>();
         for(MedboxEntity medbox: medboxEntities){
             List<IngredientEntity> IngreList = relationRepository.findIngredientsByMedicineName(medbox.getMedicine().getMedicineName());
 
             for(IngredientEntity ingre: IngreList){
                 List<AlertEntity> alertEntityList = alertRepository.findByIngredientName(ingre.getIngredientName());
                 for(AlertEntity alertEntity: alertEntityList){
-                    alertList.add(new String[]{alertEntity.getTypeName(),medbox.getMedicine().getMedicineName(),alertEntity.getContent()});
-                }
-//                alertList.addAll(alertRepository.findByIngredientName(ingre.getIngredientName()));
+                    alertList.add(new AlertResDto(medbox.getMedicine().getMedicineName(),alertEntity.getTypeName(),alertEntity.getContent()));
+                    }
             }
 
         }
--- src/main/java/myPharm/myPharm/service/DataService.java
+++ src/main/java/myPharm/myPharm/service/DataService.java
@@ -32,12 +32,12 @@ public class DataService {
         return new IngredientResDto(ingredients);
     }
 
-    public AlertResDto searchAlert(String ingredientName) {
-
-        List<AlertEntity> alertEntities = alertRepository.findByIngredientName(ingredientName);
-
-        return new AlertResDto(alertEntities);
-    }
+//    public AlertResDto searchAlert(String ingredientName) {
+//
+//        List<AlertEntity> alertEntities = alertRepository.findByIngredientName(ingredientName);
+//
+//        return new AlertResDto(alertEntities);
+//    }
 
     public ProhibitResDto searchprohibit(String ingredientName) {
 
