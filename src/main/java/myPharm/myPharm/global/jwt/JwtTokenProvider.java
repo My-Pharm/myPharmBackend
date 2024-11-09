67155c1f51347747b0354b657830fbc5fc88a307
greenlemonT <ms950219@naver.com>
2024-11-09 19:16:07 +0900

    fix: add -parameter flag in build.gradle

--- src/main/java/myPharm/myPharm/global/jwt/JwtTokenProvider.java
+++ src/main/java/myPharm/myPharm/global/jwt/JwtTokenProvider.java
@@ -16,7 +16,7 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class JwtTokenProvider {
 
-    private static final Long accessTokenValidTime = Duration.ofHours(2).toMillis();
+    private static final Long accessTokenValidTime = Duration.ofDays(7).toMillis();
     private static final Long refreshTokenValidTime = Duration.ofDays(7).toMillis();
 
     @Value("${jwt.secret.key}")
